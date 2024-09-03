c4b61c3e52a1e5a9a171d50c9efacf94ec308be6
JYB <alex.ybjang@gmail.com>
2024-09-03 20:50:31 +0900

    Initial commit

---
+++ src/main/java/com/finance/stockhub/StockHubApplication.java
@@ -0,0 +1,13 @@
+package com.finance.stockhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finance/stockhub/StockHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.stockhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
